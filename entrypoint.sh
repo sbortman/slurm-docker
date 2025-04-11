78d985fdaff059991cb4854143988ce96591a78f
sbortman <scott.bortman@gmail.com>
2025-04-11 15:25:29 -0400

    chmod entrypoint.sh to 600

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,21 @@
 set -e
 
 if [ "$SLURMDBD" = "yes" ]; then
+  echo "Fixing permissions on slurmdbd.conf..."
+  chmod 600 /etc/slurm/slurmdbd.conf
   echo "Starting slurmdbd..."
   exec slurmdbd -Dvvv
+
 elif [ "$SLURMCTLD" = "yes" ]; then
   echo "Waiting for slurmdbd to be ready..."
   sleep 5
   echo "Starting slurmctld..."
   exec slurmctld -Dvvv
+
 elif [ "$SLURMD" = "yes" ]; then
   echo "Starting slurmd..."
   exec slurmd -Dvvv
+
 else
   exec "$@"
 fi
